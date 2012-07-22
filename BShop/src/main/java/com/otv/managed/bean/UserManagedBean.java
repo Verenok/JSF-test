8f41bc142908e94f4f0c8c3f820fd8f8ce854155
Verenok <didkovskayavera@gmail.com>
2012-07-22 23:50:18 +0300

    d

--- BShop/src/main/java/com/otv/managed/bean/UserManagedBean.java
+++ BShop/src/main/java/com/otv/managed/bean/UserManagedBean.java
@@ -4,162 +4,132 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.model.SelectableDataModel;
 import org.springframework.dao.DataAccessException;
 
 import com.otv.model.User;
 import com.otv.user.service.IUserService;
 
 /**
- * 
  * User Managed Bean
- * 
- * @author onlinetechvision.com
- * @since 25 Mar 2012
- * @version 1.0.0
  *
+ * @author onlinetechvision.com
+ * @version 1.0.0
+ * @since 25 Mar 2012
  */
-@ManagedBean(name="userMB")
+@ManagedBean(name = "userMB")
 @RequestScoped
 public class UserManagedBean implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private static final String SUCCESS = "success";
-	private static final String ERROR   = "error";
-	
-	//Spring User Service is injected...
-	@ManagedProperty(value="#{UserService}")
-	IUserService userService;
-	
-	List<User> userList;
-	
-	private int id;
-	private String name;
-	private String surname;
-	
-	/**
-	 * Add User
-	 * 
-	 * @return String - Response Message
-	 */
-	public String addUser() {
-		try {
-			User user = new User();
-			user.setId(getId());
-			user.setName(getName());
-			user.setSurname(getSurname());
-			getUserService().addUser(user);
-			return SUCCESS;
-		} catch (DataAccessException e) {
-			e.printStackTrace();
-		} 	
-		
-		return ERROR;
-	}
-	
-	/**
-	 * Reset Fields
-	 * 
-	 */
-	public void reset() {
-		this.setId(0);
-		this.setName("");
-		this.setSurname("");
-	}
-	
-	/**
-	 * Get User List
-	 * 
-	 * @return List - User List
-	 */
-	public List<User> getUserList() {
-		userList = new ArrayList<User>();
-		userList.addAll(getUserService().getUsers());
-		return userList;
-	}
-	
-	/**
-	 * Get User Service
-	 * 
-	 * @return IUserService - User Service
-	 */
-	public IUserService getUserService() {
-		return userService;
-	}
 
-	/**
-	 * Set User Service
-	 * 
-	 * @param IUserService - User Service
-	 */
-	public void setUserService(IUserService userService) {
-		this.userService = userService;
-	}
-	
-	/**
-	 * Set User List
-	 * 
-	 * @param List - User List
-	 */
-	public void setUserList(List<User> userList) {
-		this.userList = userList;
-	}
-	
-	/**
-	 * Get User Id
-	 * 
-	 * @return int - User Id
-	 */
-	public int getId() {
-		return id;
-	}
+    private static final long serialVersionUID = 1L;
+    private static final String SUCCESS = "success";
+    private static final String ERROR = "error";
 
-	/**
-	 * Set User Id
-	 * 
-	 * @param int - User Id
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
+    //Spring User Service is injected...
+    @ManagedProperty(value = "#{UserService}")
+    IUserService userService;
 
-	/**
-	 * Get User Name
-	 * 
-	 * @return String - User Name
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * Set User Name
-	 * 
-	 * @param String - User Name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * Get User Surname
-	 * 
-	 * @return String - User Surname
-	 */
-	public String getSurname() {
-		return surname;
-	}
-	
-	/**
-	 * Set User Surname
-	 * 
-	 * @param String - User Surname
-	 */
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	
+    List<User> userList;
+    private UserDataModel model;
+    User selected;
+    @PostConstruct
+//   @RolesAllowed({"ADMIN"})
+   protected void postConstruct() {}  //
+    /**
+     * Add User
+     *
+     * @return String - Response Message
+     */
+    public String addUser() {
+        try {
+            //         User user = new User();
+            //        user.setId(getId());
+            //         user.setName(getName());
+            //         user.setSurname(getSurname());
+            getUserService().addUser(selected);
+            return SUCCESS;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+        return ERROR;
+    }
+
+    public void editRow(RowEditEvent event) {
+        User rowItem = (User) event.getObject();
+        getUserService().updateUser(rowItem);        //endEditItem(rowItem);
+        //saveSelected();
+        //endSelect();
+    }
+
+    /**
+     * Reset Fields
+     */
+    public void reset() {
+    }
+
+    /**
+     * Get User List
+     *
+     * @return List - User List
+     */
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public UserDataModel getModel() {
+        userList = new ArrayList<User>();
+        userList.addAll(getUserService().getUsers());
+        model = new UserDataModel(userList);
+        return model;
+    }
+
+    public void setModel(UserDataModel model) {
+        this.model = model;
+    }
+
+    /**
+     * Get User Service
+     *
+     * @return IUserService - User Service
+     */
+    public IUserService getUserService() {
+        return userService;
+    }
+
+
+    public void setUserService(IUserService userService) {
+        this.userService = userService;
+    }
+
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+
+    public User getSelected() {
+        return selected;
+    }
+
+    public void setSelected(User selected) {
+        this.selected = selected;
+    }
+
+    public String createNew() {
+        return "recept";
+    }
+
+    public String delete() {
+        return "removal";
+    }
+    public boolean isEnableDelete() {
+        return !(selected==null);
+    }
 }
---
+++ BShop/src/main/resources/META-INF/import.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO USER (ID,NAME,SURNAME) VALUES (100,'Sigizmund','Appolinarievich');
+INSERT INTO USER (ID,NAME,SURNAME) VALUES (101,'Izolda','Sigizmundovna');
